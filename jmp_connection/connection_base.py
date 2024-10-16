58afdd34ae0bca74b4e1fa566f433c614c1b5b53
Kevin Cloutier <81378386+jniordev@users.noreply.github.com>
2024-10-16 13:46:37 -0400

    moved to parent
    moved some connection methods to the connection_base parent

--- jmp_connection/connection_base.py
+++ jmp_connection/connection_base.py
@@ -1,4 +1,7 @@
 import threading
+import traceback
+from datetime import datetime
+import socket
 
 from jmp_connection.jnior_event import JniorEvent
 from jmp_connection.jmp_messages import JmpMessage
@@ -30,11 +33,82 @@ class ConnectionBase:
         self.on_auth = JniorEvent()
         self.on_message_recv = JniorEvent()
 
+    """
+    Get and set methods for the socket
+    """
+    def set_socket(self, socket):
+        if socket is None:
+            raise Exception("Socket is None")
+        if self.socket is not None:
+            raise Exception("Socket is already defined")
+        self.socket = socket
+        self.host, self.port = self.socket.getpeername()
+        self.connected()
+
+    def get_socket(self):
+        return self.socket
+
+    """
+    Connection Methods
+    """
+    def connect(self, host=None, port=None):
+        """ connect ([host[, port]])
+
+        Called to connect to a JMP server.  if a host is not provided then the saved host will be
+        used
+
+        :param host: optional to specify a new client host
+        :param port: optional to specify a port other than the default 9220
+        """
+
+        if host is not None:
+            self.host = host
+            if port is not None:
+                self.port = port
+
+        try:
+            if self.host is None:
+                raise Exception("host is not defined")
+
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect((self.host, self.port))
+
+            self.connected()
+
+            return True
+        except Exception as err:
+            print(f"{str(datetime.now())[:-3]}: unable to connect to {self.host}:{self.port} "
+                  f"because {err}\n{traceback.format_exc()}")
+            # close and nullify our socket
+            if self.socket is not None:
+                self.socket.close()
+                self.socket = None
+
+            return False
+
     def get_host_info(self):
         """
         :return: a string with information about the connection that is being used
         """
         return f"('{self.host}', {self.port})"
+
+    def close(self):
+        """
+        closes and nullifies the socket
+        """
+        self.socket_input_stream.close()
+        if self.socket is not None:
+            self.socket.close()
+            self.socket = None
+
+            # alert listener handlers that the connection has been closed
+            self.on_connection(self, connected=False, socket=self.socket)
+
+    def is_connected(self):
+        """
+        :return: whether the connection is connected
+        """
+        return self.socket is not None
 
     """
     Event Handlers
--- jmp_connection/jmp_connection.py
+++ jmp_connection/jmp_connection.py
@@ -41,58 +41,6 @@ class JMPConnection(ConnectionBase):
 
         self.console_session = None
 
-    """
-    Get and set methods for the socket
-    """
-    def set_socket(self, socket):
-        if socket is None:
-            raise Exception("Socket is None")
-        if self.socket is not None:
-            raise Exception("Socket is already defined")
-        self.socket = socket
-        self.host, self.port = self.socket.getpeername()
-        self.connected()
-
-    def get_socket(self):
-        return self.socket
-
-    """
-    Connection Methods
-    """
-    def connect(self, host=None, port=None):
-        """ connect ([host[, port]])
-
-        Called to connect to a JMP server.  if a host is not provided then the saved host will be
-        used
-
-        :param host: optional to specify a new client host
-        :param port: optional to specify a port other than the default 9220
-        """
-
-        if host is not None:
-            self.host = host
-            if port is not None:
-                self.port = port
-
-        try:
-            if self.host is None:
-                raise Exception("host is not defined")
-
-            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.socket.connect((self.host, self.port))
-
-            self.connected()
-
-            return True
-        except Exception as err:
-            print(f"{str(datetime.now())[:-3]}: unable to connect to {self.host}:{self.port} "
-                  f"because {err}\n{traceback.format_exc()}")
-            # close and nullify our socket
-            self.socket.close()
-            self.socket = None
-
-            return False
-
     def start_tls(self):
         """
         start_tls
@@ -117,24 +65,6 @@ class JMPConnection(ConnectionBase):
 
         tls_elapsed = time.time() - tls_start_time
         print(f"{str(datetime.now())[:-3]}: upgrading socket to TLS took {tls_elapsed}")
-
-    def close(self):
-        """
-        closes and nullifies the socket
-        """
-        self.socket_input_stream.close()
-        if self.socket is not None:
-            self.socket.close()
-            self.socket = None
-
-            # alert listener handlers that the connection has been closed
-            self.on_connection(self, connected=False, socket=self.socket)
-
-    def is_connected(self):
-        """
-        :return: whether the connection is connected
-        """
-        return self.socket is not None
 
     def is_authenticated(self):
         """
